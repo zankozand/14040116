79d927775059f85a3a94cc38ad267f00e114c296
zankozand <zankozandkarimi44@gmail.com>
2025-04-06 00:16:57 +0330

    Add files via upload

---
+++ hotel (1) (1).cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+struct numberroom{
+	int room;
+	int emptyroom;
+	int fullroom;
+};
+struct customer{
+char fristname[20];
+char lastname[35];
+int nationalcode;
+char mobile[10];
+int age;
+};
+int main(){
+	numberroom st;
+	int number;
+	cout<<"please enter number"<<endl;
+	cin>>number;
+	int z=10;
+	
+	
+	{
+	cout<<"please enter room"<<endl;
+	cin>>st.room;
+	cout<<"please enter emptyroom"<<endl;
+	cin>>st.emptyroom;
+	cout<<"please enter fullroom"<<endl;
+	cin>>st.fullroom;
+	}
+};
+switch(a){
+	case x:cout<<"room (x)";
+	break;
+	case y:cout<<"emptyroom (y)";
+	break;
+	case r:cout<<"fullroom (r)";
+	break; 
+};
+{
+   customer st;
+	int number;
+	cout<<"please enter number"<<endl;
+	cin>>number;
+	int i=0;
+	for(i=0; i<number;i++)
+{
+	cout<<"please enter fristname"<<endl;
+	cin>>st.fristname;
+	cout<<"please enter lastname"<<endl;
+	cin>>st.lastname;
+	cout<<"please enter nationalcode"<<endl;
+	cin>>st.nationalcode;
+	cout<<"please enter mobile"<<endl;
+	cin>>st.mobile;
+	cout<<"please enter age"<<endl;
+	cin>>st.age
+};
+switch(){
+	case 1:cout<<"fristname (1)";
+	break;
+	case 2:cout<<"lastname (2)";
+	break;
+	case 3:cout<<"nationalcode(3)";
+	break;
+	case 4:cout<<"mobile (4)";
+	break;
+	case 5:cout<<"age (5)";
+	break;
+};
+return 0;
+};
+
+
+				
